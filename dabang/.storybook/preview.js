5d415bc15f873f2735edc9509c0b8546658987dc
STHyeon <toby2009@naver.com>
2020-11-06 12:23:42 +0900

    config: set storybook link

--- dabang/.storybook/preview.js
+++ dabang/.storybook/preview.js
@@ -1,4 +1,11 @@
+import { addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+import routerDecorator from './router-decorator';
+
+addDecorator(withKnobs);
+addDecorator(routerDecorator);
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+    actions: { argTypesRegex: '^on[A-Z].*' }
+};
