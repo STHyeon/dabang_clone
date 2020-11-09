efd6eee7548c99eb43a5772e7ae67c34b87aedf0
STHyeon <toby2009@naver.com>
2020-11-09 18:02:35 +0900

    add: atomic Img

---
+++ dabang/src/components/UI/atoms/Img/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from './style';
+
+export interface ImgProps {
+    src: any; // 이미지 주소
+    alt: string; // 이미지 설명
+}
+
+export function Img({ ...props }: ImgProps): React.ReactElement {
+    return <S.Img {...props} />;
+}
