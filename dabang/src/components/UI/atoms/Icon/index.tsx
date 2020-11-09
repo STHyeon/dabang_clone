19b69f5690b7e147adb373ff94d355b67e48f494
STHyeon <toby2009@naver.com>
2020-11-09 18:02:52 +0900

    add: atomic Icon

---
+++ dabang/src/components/UI/atoms/Icon/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as S from './style';
+
+// type A<T> = {
+//     A:
+// }
+
+export interface IconProps {
+    src: any; // 이미지 주소
+    alt: string; // 이미지 설명
+    height?: string;
+}
+
+export function Icon({ height = '2rem', ...props }: IconProps): React.ReactElement {
+    return <S.ImgStyle height={height} {...props} />;
+}
---
+++ dabang/src/components/UI/atoms/Icon/style.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ImgStyle = styled.img`
+    height: ${(props) => props.height};
+`;
