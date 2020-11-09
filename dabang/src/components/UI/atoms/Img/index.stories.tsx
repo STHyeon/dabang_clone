33083377792e22463aa3818fd0beb52111d0f04c
STHyeon <toby2009@naver.com>
2020-11-09 18:03:35 +0900

    add: atomic Img

---
+++ dabang/src/components/UI/atoms/Img/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Img, ImgProps } from './index';
+import LogoSvg from '../../../../assets/images/logo.svg';
+
+export default {
+    title: 'Atoms/Img'
+} as Meta;
+
+const Template: Story<ImgProps> = (args) => <Img {...args} />;
+
+export const general = Template.bind({});
+general.args = {
+    src: LogoSvg,
+    alt: '로고'
+};
