13388c78d860a1770019cba7bc8e1ed9a3b1a2b2
STHyeon <toby2009@naver.com>
2020-11-11 17:59:50 +0900

    update: add molecules

--- dabang/src/components/UI/index.ts
+++ dabang/src/components/UI/index.ts
@@ -3,3 +3,10 @@ export { Icon } from './atoms/Icon';
 export { Img } from './atoms/Img';
 export { Input } from './atoms/Input';
 export { EventDate1, EventDate2 } from './atoms/EventDate';
+export { Label } from './atoms/Label';
+export { GNBItem } from './atoms/GNBItem';
+
+export { Card1, Card2, Card3, Card4 } from './molecules/Card';
+export { FormInput } from './molecules/FormInput';
+export { GNBType1, GNBType2, GNBType3 } from './molecules/GNB';
+export { ImgBtn } from './molecules/ImgBtn';
