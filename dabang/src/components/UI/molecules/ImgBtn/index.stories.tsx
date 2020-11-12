bd14a762b4fa6e4e3cabc59becd3da6698206df4
STHyeon <toby2009@naver.com>
2020-11-12 18:02:53 +0900

    update: storybook Imgbtn

--- dabang/src/components/UI/molecules/ImgBtn/index.stories.tsx
+++ dabang/src/components/UI/molecules/ImgBtn/index.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { ImgBtn, ImgBtnProps } from './index';
+import TestImg from 'assets/images/test.png';
 
 export default {
     title: 'Molecules/ImgBtn',
@@ -11,3 +12,8 @@ export default {
 const Template: Story<ImgBtnProps> = (args) => <ImgBtn {...args} />;
 
 export const general = Template.bind({});
+general.args = {
+    src: `${TestImg}`,
+    alt: 'test',
+    styletype: 'transparent'
+};
