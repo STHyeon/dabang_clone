bdd8e16a9f3f95de0bb8d0cc598d2347f38f6987
STHyeon <toby2009@naver.com>
2020-11-13 16:26:13 +0900

    add: new support file

--- dabang/src/utils/contents/String.ts
+++ dabang/src/utils/contents/String.ts
@@ -5,7 +5,11 @@ export const HEADER3 = ['회원가입 · 로그인'];
 export const SEEROOM = '최근 본 방';
 export const LOVEROOM = '찜한 방';
 export const ROOMDESCRIPTION = '최근에 본 방과 찜한 방을 볼 수 있어요';
+export const MORESEEROOMTEXT = '아직 못 본 더 많은 방이 있어요.';
+export const RECENTLYROOMTEXT = '최근 본 지역의 다른 방을 찾아보세요';
 
 export const SEECOMPLEX = '최근 본 단지';
 export const LOVECOMPLEX = '찜한 단지';
 export const COMPLEXDESCRIPTION = '최근에 본 단지와 찜한 단지를 볼 수 있어요';
+export const MORESEECOMPLEXTEXT = '아직 못 본 더 많은 단지가 있어요.';
+export const RECENTLYCOMPLEXTEXT = '최근 본 지역의 다른 단지를 찾아보세요';
