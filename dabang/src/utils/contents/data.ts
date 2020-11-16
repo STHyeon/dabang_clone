0883779b6b334fc659ae839970b7a2d48592875c
STHyeon <toby2009@naver.com>
2020-11-16 14:52:46 +0900

    add: test data

---
+++ dabang/src/utils/contents/data.ts
@@ -0,0 +1,45 @@
+import TestImg from 'assets/images/test.png';
+
+const Now = new Date();
+
+export const Type01CardData = [
+    {
+        CardType: 'type02',
+        title: 'test',
+        FlagValid: true,
+        Flag: '플러스',
+        room: '오피스텔',
+        roomInfo: '래미안',
+        to: '/',
+        imgSrc: `${TestImg}`,
+        description1: '반지층, 라라라',
+        description2: '밥밥지지츠츠',
+        chkDate: Now
+    },
+    {
+        CardType: 'type02',
+        title: 'test',
+        FlagValid: true,
+        Flag: '플러스',
+        room: '오피스텔',
+        roomInfo: '래미안',
+        to: '/',
+        imgSrc: `${TestImg}`,
+        description1: '반지층, 라라라',
+        description2: '밥밥지지츠츠',
+        chkDate: Now
+    },
+    {
+        CardType: 'type02',
+        title: 'test',
+        FlagValid: true,
+        Flag: '플러스',
+        room: '오피스텔',
+        roomInfo: '래미안',
+        to: '/',
+        imgSrc: `${TestImg}`,
+        description1: '반지층, 라라라',
+        description2: '밥밥지지츠츠',
+        chkDate: Now
+    }
+];
