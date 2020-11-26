f5824d0789facdbcbe1b9820f67f37ccbc93bbe1
STHyeon <toby2009@naver.com>
2020-11-26 17:10:20 +0900

    update: 키워드 지도

--- client/src/components/atoms/KakaoMap/index.tsx
+++ client/src/components/atoms/KakaoMap/index.tsx
@@ -8,6 +8,7 @@ export interface KakaoMapProps {
     longitude: number;
     height?: string;
 }
+
 export function KakaoMap({
     latitude,
     longitude,
@@ -17,19 +18,29 @@ export function KakaoMap({
 
     useEffect(() => {
         const kakaoMapElement = kakaoMapRef.current;
-        if (kakaoMapElement) {
-            const options = {
-                center: new kakao.maps.LatLng(latitude, longitude),
-                level: 3
-            };
-            const kakaoMap = new kakao.maps.Map(kakaoMapElement, options);
-            const markerPosition = new kakao.maps.LatLng(latitude, longitude);
-            const marker = new kakao.maps.Marker({
-                position: markerPosition
-            });
+        const options = {
+            center: new kakao.maps.LatLng(37.566826, 126.9786567),
+            level: 3
+        };
+        const kakaoMap = new kakao.maps.Map(kakaoMapElement, options);
+        const ps = new kakao.maps.services.Places();
 
-            marker.setMap(kakaoMap);
+        function placesSearchCB(data: any, status: any, pagination: any) {
+            if (status === kakao.maps.services.Status.OK) {
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+                // LatLngBounds 객체에 좌표를 추가합니다
+                var bounds = new kakao.maps.LatLngBounds();
+
+                for (var i = 0; i < data.length; i++) {
+                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                }
+
+                // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+                kakaoMap.setBounds(bounds);
+            }
         }
+
+        ps.keywordSearch('신정역', placesSearchCB);
     }, [latitude, longitude]);
 
     return <S.Container ref={kakaoMapRef} height={height} />;
