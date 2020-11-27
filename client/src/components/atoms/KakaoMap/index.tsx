d6108ba2e2eaabdf5e3c4677e9740b3b5e26eaa8
STHyeon <toby2009@naver.com>
2020-11-27 14:14:49 +0900

    add: kakao map clusterer

--- client/src/components/atoms/KakaoMap/index.tsx
+++ client/src/components/atoms/KakaoMap/index.tsx
@@ -48,24 +48,23 @@ export function KakaoMap({
 
         ps.keywordSearch(address, placesSearchCB);
 
-        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
-
         if (positions) {
-            for (var i = 0; i < positions.length; i++) {
-                // 마커 이미지의 이미지 크기 입니다
-                var imageSize = new kakao.maps.Size(24, 35);
+            // 마커 클러스터러를 생성합니다
+            var clusterer = new kakao.maps.MarkerClusterer({
+                map: kakaoMap, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+                averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+                minLevel: 8 // 클러스터 할 최소 지도 레벨
+            });
 
-                // 마커 이미지를 생성합니다
-                var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-                // 마커를 생성합니다
-                new kakao.maps.Marker({
-                    map: kakaoMap, // 마커를 표시할 지도
-                    position: new kakao.maps.LatLng(positions[i].latitude, positions[i].longitude), // 마커를 표시할 위치
-                    title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                    image: markerImage // 마커 이미지
+            // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
+            var markers = positions.map(function (position: any, i: any) {
+                return new kakao.maps.Marker({
+                    position: new kakao.maps.LatLng(position.latitude, position.longitude)
                 });
-            }
+            });
+
+            // 클러스터러에 마커들을 추가합니다
+            clusterer.addMarkers(markers);
         }
     }, [address, positions]);
 
