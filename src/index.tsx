7ca676615acc746e0f1abbd36d033e9438f6d05d
STHyeon <toby2009@naver.com>
2020-11-30 14:58:31 +0900

    fix: css 깨짐 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'assets/scss/project.scss';
 
 ReactDOM.render(
     <React.StrictMode>
