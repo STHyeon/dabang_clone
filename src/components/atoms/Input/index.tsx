8ece02172d0072168688b3a13defbab8b45bbcd9
STHyeon <toby2009@naver.com>
2021-01-07 17:06:02 +0900

    update: Clean Code - Input

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -3,14 +3,14 @@ import * as S from './style';
 
 export interface InputProps {
     id?: string; // 접근성
+    inputType?: string; // input type 설정
     description?: string; // input 설명 (접근성)
     invalid?: boolean; // is invalid
     disabled?: boolean;
-    defaultValue?: string;
-    placeholder?: string;
+    defaultValue?: string; // 기본값
+    placeholder?: string; // 힌트
     value?: string | number;
-    inputHeight?: string;
-    inputType?: string;
+    height?: string;
 
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
     onKeyPress?: (e: React.KeyboardEvent) => void;
@@ -18,18 +18,6 @@ export interface InputProps {
 
 export function Input({ id, description, inputType, ...props }: InputProps): React.ReactElement {
     return (
-        <>
-            {inputType ? (
-                <S.Input
-                    type={inputType}
-                    id={id}
-                    title={description}
-                    autoComplete="off"
-                    {...props}
-                />
-            ) : (
-                <S.Input type="text" id={id} title={description} autoComplete="off" {...props} />
-            )}
-        </>
+        <>{inputType ? <S.Input type={inputType} id={id} title={description} autoComplete="off" {...props} /> : <S.Input type="text" id={id} title={description} autoComplete="off" {...props} />}</>
     );
 }
